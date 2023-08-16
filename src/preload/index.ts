423bb471a586ca230d4917f7e0b01c788191f72b
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-08-16 21:11:14 +0800

    refactor: Refactoring preload.
    Move events proxy handlers to instance.

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,18 +1,9 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-import type { ResponseEventMap } from '../interfaces/response-event-map'
-import type { RequestEventMap } from '../interfaces/request-event-map'
-import { TypedEventEmitter } from '../utilities/typed-event-emitter'
+import { Router } from './router'
 
-const requestEventEmitter = new TypedEventEmitter<RequestEventMap>()
-const responseEventEmitter = new TypedEventEmitter<ResponseEventMap>()
-// const responseEventEmitter = new EventEmitter<ResponseEventMap>()
-
-console.log('responseEventEmitter', responseEventEmitter)
-
-// const response = (event: string, ...args: unknown[]): boolean =>
-//   responseEventEmitter.emit(event, ...args)
+const router = new Router()
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
@@ -20,9 +11,8 @@ console.log('responseEventEmitter', responseEventEmitter)
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('requestEventEmitter', requestEventEmitter)
-    contextBridge.exposeInMainWorld('responseEventEmitter', responseEventEmitter)
-    // contextBridge.exposeInMainWorld('response', response)
+    contextBridge.exposeInMainWorld('requestEventEmitter', router.requestEventEmitter)
+    contextBridge.exposeInMainWorld('responseEventEmitter', router.responseEventEmitter)
   } catch (error) {
     console.error(error)
   }
@@ -36,31 +26,3 @@ if (process.contextIsolated) {
   // @ts-ignore (define in dts)
   // window.response = response
 }
-
-ipcRenderer.on('connection', (_, clientId: string) => {
-  requestEventEmitter.emit('connection', clientId)
-})
-
-ipcRenderer.on('disconnect', (_, clientId: string) => {
-  requestEventEmitter.emit('disconnect', clientId)
-})
-
-ipcRenderer.on('answer', (_, clientId: string, description: RTCSessionDescriptionInit) => {
-  console.log('[ipcRenderer->requestEventEmitter]', 'answer', clientId, description)
-  requestEventEmitter.emit('answer', clientId, description)
-})
-
-ipcRenderer.on('icecandidate', (_, clientId: string, candidate: RTCIceCandidate) => {
-  console.log('[ipcRenderer->requestEventEmitter]', 'icecandidate', clientId, candidate)
-  requestEventEmitter.emit('icecandidate', clientId, candidate)
-})
-
-responseEventEmitter.on('icecandidate', (clientId: string, candidate: RTCIceCandidateInit) => {
-  console.log('[responseEventEmitter->ipcRenderer]', 'icecandidate', clientId, candidate)
-  ipcRenderer.send('icecandidate', clientId, candidate)
-})
-
-responseEventEmitter.on('offer', (clientId: string, description: RTCSessionDescriptionInit) => {
-  console.log('[responseEventEmitter->ipcRenderer]', 'offer', clientId, description)
-  ipcRenderer.send('offer', clientId, description)
-})
